11b5e435e92cd8cb2628940276a57a39fc27e690
Maria Savina <mpsavina@gmail.com>
2019-04-13 06:59:24 +0300

    Extract studio list page container into a separate module

---
+++ src/js/containers/studiosListPage.jsx
@@ -0,0 +1,97 @@
+import React from "react";
+
+import { StudioFilters } from "../components/studioFilters";
+import { StudioListing } from "../components/studioListing";
+
+class StudiosListPage extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            data: [],
+            numTotal: null,
+            searchQuery: "",
+            filterLocation: null,
+            filterStyle: null,
+            filterRating: null
+        };
+    }
+
+    handleQueryChange(newQuery) {
+        this.setState({
+            searchQuery: newQuery
+        });
+    }
+
+    handleLocationChange(newLocation) {
+        this.setState({
+            filterLocation: newLocation
+        });
+    }
+
+    handleStyleChange(newStyle) {
+        this.setState({
+            filterStyle: newStyle
+        });
+    }
+
+    handleRatingChange(newRating) {
+        this.setState({
+            filterRating: newRating
+        });
+    }
+
+    componentDidMount() {
+        fetch("/v1/studios/los-angeles").then(response => {
+            return response.json();
+        }).then(data => {
+            this.setState({
+                data: data.studios,
+                numTotal: data.numTotal
+            });
+        }).catch(error => {
+            console.log(error);
+        });
+    }
+
+    render() {
+        let filteredData = [];
+
+        this.state.data.forEach(item => {
+            if (item.title.toLowerCase().includes(this.state.searchQuery.toLowerCase())) {
+                if (item.location === this.state.filterLocation || this.state.filterLocation === null) {
+                    if (item.tags.includes(this.state.filterStyle) || this.state.filterStyle === null) {
+                        if (item.rating >= this.state.filterRating) {
+                            filteredData.push(item);
+                        }
+                    }
+                }
+            }
+        });
+
+        return (
+            <>
+                <StudioFilters onQueryChange={q => this.handleQueryChange(q)}
+                               onLocationChange={q => this.handleLocationChange(q)}
+                               onStyleChange={q => this.handleStyleChange(q)}
+                               onRatingChange={q => this.handleRatingChange(q)} />
+
+
+                <div className="postings-count">
+                    Найдено <span>{ this.state.numTotal }</span> студий
+                </div>
+
+
+                <div className={'postings-container'}>
+                    { filteredData.map(datum => <StudioListing content={datum} />) }
+                </div>
+
+                <div id="postings-more">
+                    <a href="#">Следующие 20</a>
+                </div>
+            </>
+        )
+    }
+}
+
+export { StudiosListPage };
--- src/js/index.jsx
+++ src/js/index.jsx
@@ -4,108 +4,12 @@ import ReactDOM from "react-dom";
 
 import styled from 'styled-components';
 
-import { subwayMoscow } from "./util/subway";
-
 import { LoginCorner } from "./containers/loginCorner";
-import { RatingSlider } from "./components/ratingSlider";
-
-import { StudioListing } from "./components/studioListing";
-import { StudioFilters } from "./components/studioFilters";
-
-class ListingsPage extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      data: [],
-      numTotal: null,
-      searchQuery: "",
-      filterLocation: null,
-      filterStyle: null,
-      filterRating: null
-    };
-  }
-
-  handleQueryChange(newQuery) {
-    this.setState({
-      searchQuery: newQuery
-    });
-  }
-
-  handleLocationChange(newLocation) {
-    this.setState({
-      filterLocation: newLocation
-    });
-  }
-
-  handleStyleChange(newStyle) {
-    this.setState({
-      filterStyle: newStyle
-    });
-  }
-
-  handleRatingChange(newRating) {
-    this.setState({
-      filterRating: newRating
-    });
-  }
-
-  componentDidMount() {
-    fetch("/v1/studios/los-angeles").then(response => {
-      return response.json();
-    }).then(data => {
-      this.setState({
-        data: data.studios,
-        numTotal: data.numTotal
-      });
-    }).catch(error => {
-      console.log(error);
-    });
-  }
-
-  render() {
-    let filteredData = [];
-
-    this.state.data.forEach(item => {
-      if (item.title.toLowerCase().includes(this.state.searchQuery.toLowerCase())) {
-        if (item.location === this.state.filterLocation || this.state.filterLocation === null) {
-          if (item.tags.includes(this.state.filterStyle) || this.state.filterStyle === null) {
-            if (item.rating >= this.state.filterRating) {
-              filteredData.push(item);
-            }
-          }
-        }
-      }
-    });
-
-    return (
-        <>
-          <StudioFilters onQueryChange={q => this.handleQueryChange(q)}
-                         onLocationChange={q => this.handleLocationChange(q)}
-                         onStyleChange={q => this.handleStyleChange(q)}
-                         onRatingChange={q => this.handleRatingChange(q)} />
-
-
-          <div className="postings-count">
-            Найдено <span>{ this.state.numTotal }</span> студий
-          </div>
-
-
-          <div className={'postings-container'}>
-            { filteredData.map(datum => <StudioListing content={datum} />) }
-          </div>
-
-          <div id="postings-more">
-            <a href="#">Следующие 20</a>
-          </div>
-        </>
-    )
-  }
-}
+import { StudiosListPage } from "./containers/studiosListPage";
 
 window.addEventListener('load', () => {
     ReactDOM.render(<LoginCorner/>, document.querySelector('#login-corner'));
-    ReactDOM.render(<ListingsPage/>, document.querySelector('#listings'));
+    ReactDOM.render(<StudiosListPage/>, document.querySelector('#listings'));
 
     const gMapCanvas = document.getElementById('gmap_canvas');
 
