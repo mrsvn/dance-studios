b7514e14e23a9a139bd94ff533b36bde025773c2
Maria Savina <mpsavina@gmail.com>
2019-04-28 02:28:03 +0300

    Restore styles in UserCalendar

--- src/js/containers/userCalendar.jsx
+++ src/js/containers/userCalendar.jsx
@@ -1,5 +1,27 @@
 import React from "react";
 
+import styled from "styled-components";
+
+const CalendarDiv = styled.div`
+    display: flex;
+
+    .day-column {
+        border-right: 1px #dddddd solid;
+        flex-grow: 1;
+    }
+
+    #time-column > div {
+        font-size: 10px;
+        height: 100px;
+    }
+
+    .day-header, #time-column .day-header {
+        height: 1.5em;
+        font-size: 1.5em;
+        text-align: center;
+    }
+`;
+
 class UserCalendar extends React.Component {
     constructor(props) {
         super(props);
@@ -62,48 +84,45 @@ class UserCalendar extends React.Component {
             day.setDate(day.getDate() + 1);
         }
 
-        return (
-            <div id="calendar">
-                {/*// <!--				<div class="calendar-modes">-->*/}
-                {/*// <!--					<a href="#">месяц</a>-->*/}
-                {/*// <!--					<a href="#">неделя</a>-->*/}
-                {/*// <!--					<a href="#">списком</a>-->*/}
-                {/*// <!--				</div>-->*/}
-                {/*// <!--				<div style="height: 1000px; font-size: 40px; background: pink;">-->*/}
-                {/*// <!--					календарь-->*/}
-                {/*// <!--				</div>-->*/}
-                <div id="time-column">
-                    <div className="day-header"> </div>
-                    <div>00:00</div>
-                    <div>01:00</div>
-                    <div>02:00</div>
-                    <div>03:00</div>
-                    <div>04:00</div>
-                    <div>05:00</div>
-                    <div>06:00</div>
-                    <div>07:00</div>
-                    <div>08:00</div>
-                    <div>09:00</div>
-                    <div>10:00</div>
-                    <div>11:00</div>
-                    <div>12:00</div>
-                    <div>13:00</div>
-                    <div>14:00</div>
-                    <div>15:00</div>
-                    <div>16:00</div>
-                    <div>17:00</div>
-                    <div>18:00</div>
-                    <div>19:00</div>
-                    <div>20:00</div>
-                    <div>21:00</div>
-                    <div>22:00</div>
-                    <div>23:00</div>
-                </div>
-
-                {dayColumns}
-
+        return <CalendarDiv>
+            {/*// <!--				<div class="calendar-modes">-->*/}
+            {/*// <!--					<a href="#">месяц</a>-->*/}
+            {/*// <!--					<a href="#">неделя</a>-->*/}
+            {/*// <!--					<a href="#">списком</a>-->*/}
+            {/*// <!--				</div>-->*/}
+            {/*// <!--				<div style="height: 1000px; font-size: 40px; background: pink;">-->*/}
+            {/*// <!--					календарь-->*/}
+            {/*// <!--				</div>-->*/}
+            <div id="time-column">
+                <div className="day-header"> </div>
+                <div>00:00</div>
+                <div>01:00</div>
+                <div>02:00</div>
+                <div>03:00</div>
+                <div>04:00</div>
+                <div>05:00</div>
+                <div>06:00</div>
+                <div>07:00</div>
+                <div>08:00</div>
+                <div>09:00</div>
+                <div>10:00</div>
+                <div>11:00</div>
+                <div>12:00</div>
+                <div>13:00</div>
+                <div>14:00</div>
+                <div>15:00</div>
+                <div>16:00</div>
+                <div>17:00</div>
+                <div>18:00</div>
+                <div>19:00</div>
+                <div>20:00</div>
+                <div>21:00</div>
+                <div>22:00</div>
+                <div>23:00</div>
             </div>
-        )
+
+            {dayColumns}
+        </CalendarDiv>;
     }
 }
 
